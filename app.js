55dc1fd7c7269854759c12ec0f49702098170385
anton <anton@freshbooks.com>
2014-03-23 08:04:04 -0400

    Fixed up the favicon

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(favicon());
+app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
